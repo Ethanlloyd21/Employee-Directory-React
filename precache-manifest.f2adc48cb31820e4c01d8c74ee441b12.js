9fed384a60c2b9223d1a594f5ffcb3aaf5797ecf
Lloyd Marcelino <lloydmarcelino@gmail.com>
2020-04-12 14:20:06 -0700

    Updates

---
+++ precache-manifest.f2adc48cb31820e4c01d8c74ee441b12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a90500904744cc71bf559d1629462c1b",
+    "url": "/employee-directory-react/index.html"
+  },
+  {
+    "revision": "ebacec7fbf094ae155da",
+    "url": "/employee-directory-react/static/css/main.e1f4db5f.chunk.css"
+  },
+  {
+    "revision": "4bf7a90a46b61edd1e61",
+    "url": "/employee-directory-react/static/js/2.ba481d3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory-react/static/js/2.ba481d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebacec7fbf094ae155da",
+    "url": "/employee-directory-react/static/js/main.450c4910.chunk.js"
+  },
+  {
+    "revision": "ede7cf2ae0d601b5c312",
+    "url": "/employee-directory-react/static/js/runtime-main.8f7766e9.js"
+  }
+]);
--- src/utils/API.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-const BASEURL = `https://randomuser.me/api/?nat=us&results=100`;
-
-export default {
-    search() {
-        return axios.get(BASEURL);
-    },
-};
